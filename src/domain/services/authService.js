bb189bc2de19eae7fd055a0aad113a58ea40bebe
Brayan Hincapie Monsalve <hincapieb17@gmail.com>
2025-05-04 13:28:04 -0500

    feat(auth): create auth controller and service

---
+++ src/domain/services/authService.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const userService = require('./userService');
+
+const authService = {
+
+    loginUser: async (email, password) => {
+
+        const user = await userService.getUserByEmail(email);
+        if (!user) {
+            throw new Error('Credenciales inválidas');
+        }
+    
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if (!isMatch) {
+            throw new Error('Credenciales inválidas');
+        }
+    
+        // Generar el token JWT
+        const token = jwt.sign(
+            { id: user.id, email: user.email },
+            'clave_secreta', 
+            { expiresIn: '1h' }
+        );
+
+        return token;
+        
+    }
+}
+
+module.exports = authService;
