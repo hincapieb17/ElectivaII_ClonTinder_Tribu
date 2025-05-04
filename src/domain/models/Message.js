c82ab5e50ee8a2046622f052ee9fb4a2fc2de7b8
Brayan Hincapie Monsalve <hincapieb17@gmail.com>
2025-05-04 12:46:38 -0500

    chore(message): update message model

--- src/domain/models/Message.js
+++ src/domain/models/Message.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const MenssageSchema = new mongoose.Schema({
   sender_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' },
   receiver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' },
-  contenido: String,
+  content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Mensaje', MensajeSchema);
+module.exports = mongoose.model('Mensaje', MenssageSchema);
