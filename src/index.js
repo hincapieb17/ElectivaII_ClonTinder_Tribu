848da810b9fee9489ffcc08bfe25a16243c22815
Brayan Hincapie Monsalve <hincapieb17@gmail.com>
2025-05-04 13:22:39 -0500

    chore: index updater

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 const express = require('express');
+const cors = require('cors');
+
 const userRoutes = require('./application/routes/userRouters');
 const swipeRoutes = require('./application/routes/swipeRouters');
 const matchRoutes = require('./application/routes/matchRouters');
+const messageRoutes = require('./application/routes/messageRouters');
 const swaggerDocs = require('./infrastructure/config/swaggerConfig');
-
 const conectarBD = require('./infrastructure/config/db');
 
 
 const app = express();
+
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
-
-// Inicializar Swagger
+  
+// Swagger
 swaggerDocs(app);
 
-// Conectar a BD
+// Conectar BD
 conectarBD();
 
-// Routes users
+// Rutas
 app.use('/api', userRoutes);
-// Routes Swipe
 app.use('/api', swipeRoutes);
-//Routes match
 app.use('/api', matchRoutes);
+app.use('/api', messageRoutes);
+
 
 
 app.listen(PORT, () => {
-    console.log(`Corriendo en el puerto ${PORT}`);
-})
-
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
